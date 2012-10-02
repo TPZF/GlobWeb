198127cb21058934e6afb6ff41d9212458b55c34
mshubin <maxime.shubin@telespazio.com>
2012-10-02 16:27:38 +0200

    Add arguments to predefine response and build folder path

--- build/build.py
+++ build/build.py
@@ -1,6 +1,7 @@
 import jscompiler
 import os
 import os.path
+import sys
 
 advanced = True
 
@@ -44,7 +45,7 @@ globFiles = [
 		"MercatorTiling.js",
 		"WMSLayer.js",
 		"WMSElevationLayer.js",
-        "WCSElevationLayer.js",
+		"WCSElevationLayer.js",
 		"BingLayer.js",
 		"OSMLayer.js", 
 		"BasicElevationLayer.js", 
@@ -77,6 +78,30 @@ experimentalFiles = [
 		"PolygonRenderable.js",
     ]
 
+response = 0
+
+def clear():
+	os.system('cls')
+
+# changed in case of call from other repertory
+buildPath = './'
+if len(sys.argv) > 2:
+	# predefined response
+	response = int(sys.argv[1])
+	buildPath = sys.argv[2]
+else:
+	while (response < 1 or response > 4) :  
+		print "Compilation modes : \n"
+		print "\t1 - Basic GlobWeb only"
+		print "\t2 - Basic AstroWeb only"
+		print "\t3 - Basic GlobWeb + AstroWeb"
+		print "\t4 - Basic and experimental GlobWeb + AstroWeb"
+		try:
+			response = int(raw_input("\nEnter your choice: "))
+		except ValueError: 
+			pass
+		clear()
+
 sourceFiles = []
 
 def makeSourceFiles(packageArray, exportFile):
@@ -92,59 +117,43 @@ def makeSourceFiles(packageArray, exportFile):
 			sourceFiles.extend(package)
 		
 		# Add ../src to normal source files
-		sourceFiles = map(lambda file: "../src/"+file, sourceFiles)
+		sourceFiles = map(lambda file: buildPath+"../src/"+file, sourceFiles)
 		
 		# Add GlobWeb namespaces
-		sourceFiles.insert( 0, "GlobWeb.ns.js" )
+		sourceFiles.insert( 0, buildPath+"GlobWeb.ns.js" )
 		
 		# Add export
 		if advanced:
 			sourceFiles.append(exportFile)
 
-response = 0
-
-def clear():
-	os.system('cls')
-
-while (response < 1 or response > 4) :  
-	print "Compilation modes : \n"
-	print "\t1 - Basic GlobWeb only"
-	print "\t2 - Basic AstroWeb only"
-	print "\t3 - Basic GlobWeb + AstroWeb"
-	print "\t4 - Basic and experimental GlobWeb + AstroWeb"
-	try:
-		response = int(raw_input("\nEnter your choice: "))
-	except ValueError: 
-		pass
-	clear()
 
 # array of arrays files
 package = []
 
 if response == 1:
 	package.append(globFiles)
-	makeSourceFiles(package, "GlobWeb.export.js")
+	makeSourceFiles(package, buildPath+"GlobWeb.export.js")
 	outputFilename="GlobWeb.min.js"
 elif response == 2:
 	package.append(astroFiles)
-	makeSourceFiles(package, "AstroWeb.export.js")
+	makeSourceFiles(package, buildPath+"AstroWeb.export.js")
 	outputFilename="AstroWeb.min.js"
 elif response == 3:
 	package.append(globFiles)
 	package.append(astroFiles)
-	makeSourceFiles(package, "Total.export.js")
+	makeSourceFiles(package, buildPath+"Total.export.js")
 	outputFilename="GlobWeb.min.js"
 elif response == 4:
 	package.append(globFiles)
 	package.append(astroFiles)
 	package.append(experimentalFiles)
-	makeSourceFiles(package, "Total.export.js")
+	makeSourceFiles(package, buildPath+"Total.export.js")
 	outputFilename="TotalWeb.min.js"
 
 
 # Setup compiler flags
 compilerFlags = [
-    "--externs", "externs.js",
+    "--externs", buildPath+"externs.js",
     "--language_in", "ECMASCRIPT5"
    ]
 
@@ -157,13 +166,13 @@ else:
         "--formatting", "pretty_print"])
 
 print "Building minized file."
-minimized, error = jscompiler.Compile('compiler.jar',sourceFiles,compilerFlags)
+minimized, error = jscompiler.Compile(buildPath + 'compiler.jar',sourceFiles,compilerFlags)
 print error
 
 print "Writing to %s." % outputFilename
-licence = file("licence.txt","r+").read()
-if not os.path.exists("generated"):
-    os.mkdir("generated")
+licence = file(buildPath+"licence.txt","r+").read()
+if not os.path.exists(buildPath+"generated"):
+    os.mkdir(buildPath+"generated")
 file("generated/" + outputFilename,"w").write(licence+minimized)
 
 raw_input("Press a key to finish.")
