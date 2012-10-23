d71d1c9ffdfd09d1b983a5c8aa9b398a66656caa
mshubin <maxime.shubin@telespazio.com>
2012-10-23 16:09:00 +0200

    Render without GlobWeb.Mesh

--- src/SimpleLineRenderer.js
+++ src/SimpleLineRenderer.js
@@ -27,8 +27,7 @@ GlobWeb.SimpleLineRenderer = function(tileManager)
 {
 	this.renderContext = tileManager.renderContext;
 	
-	// Renderables
-	this.shapes = [];
+	this.renderables = [];
 	
 	var vertexShader = "\
 	attribute vec3 vertex;\n\
@@ -63,42 +62,85 @@ GlobWeb.SimpleLineRenderer = function(tileManager)
  */
 GlobWeb.SimpleLineRenderer.prototype.addGeometry = function(geometry, style){
 	
+	var gl = this.renderContext.gl;
+	
 	// Create renderable
-	var shape = {
-		mesh : null,
+	var renderable = {
 		geometry : geometry,
+		vertexBuffer : gl.createBuffer(),
+		indexBuffer : gl.createBuffer(),
 		style : style
 	}
 	
-	shape.mesh = new GlobWeb.Mesh(this.renderContext);
+	switch ( geometry.type )
+	{
+		case "Polygon" :
+			// Fill array by line shape coordinates
+			
+			this.buildVertices( renderable, geometry['coordinates'][0] );
+			
+			// Compute the indices corresponding to line shape
+			var indices = [];
+			for ( var i=0; i<geometry['coordinates'][0].length-1; i++ )
+			{
+				indices.push(i);
+				indices.push(i+1);
+			}
+			// Connect last point with the first one
+			indices.push(i);
+			indices.push(0);
+			
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, renderable.indexBuffer);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+			renderable.indexBuffer.itemSize = 1;
+			renderable.indexBuffer.numItems = indices.length;
+			break;
+		case "LineString":
+			// TODO never tested
+			
+			this.buildVertices( geometry['coordinates'] );
+			
+			// Compute the indices corresponding to line shape
+			var indices = [];
+			for ( var i=0; i<geometry['coordinates'].length-1; i++ )
+			{
+				indices.push(i);
+				indices.push(i+1);
+			}
+			
+			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, renderable.indexBuffer);
+			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+			renderable.indexBuffer.itemSize = 1;
+			renderable.indexBuffer.numItems = indices.length;
+			
+			break;
+		default:
+			console.log("Not implemented yet");
+			break;
+	}
 	
-	// Fill array by line shape coordinates
+	// Add to renderables
+	this.renderables.push(renderable);
+}
+
+/**************************************************************************************************************/
+
+GlobWeb.SimpleLineRenderer.prototype.buildVertices = function( renderable, coordinates )
+{
+	var gl = this.renderContext.gl;
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, renderable.vertexBuffer);
 	var vertices = [];
-	for ( var i=0; i<geometry['coordinates'][0].length; i++)
+	for ( var i=0; i<coordinates.length; i++)
 	{
 		var pos3d = [];
-		GlobWeb.CoordinateSystem.fromGeoTo3D(geometry['coordinates'][0][i], pos3d);
-// 		vertices = vertices.concat([ geometry['coordinates'][i], geometry['coordinates'][i+1], geometry['coordinates'][i+2] ]);
+		GlobWeb.CoordinateSystem.fromGeoTo3D(coordinates[i], pos3d);
 		vertices = vertices.concat(pos3d);
 	}
 	
-	// Compute the indices corresponding to line shape
-	var indices = [];
-	for ( var i=0; i<geometry['coordinates'][0].length-1; i++ )
-	{
-		indices.push(i);
-		indices.push(i+1);
-	}
-	// Connect last point with the first one
-	indices.push(i);
-	indices.push(0);
-	
-	shape.mesh.setVertices(vertices);
-	shape.mesh.setIndices(indices);
-	shape.mesh.mode = this.renderContext.gl.LINES;
-	
-	// Add to renderables
-	this.shapes.push(shape);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	renderable.vertexBuffer.itemSize = 3;
+	renderable.vertexBuffer.numItems = vertices.length/3;
 }
 
 /**************************************************************************************************************/
@@ -108,11 +150,11 @@ GlobWeb.SimpleLineRenderer.prototype.addGeometry = function(geometry, style){
  */
 GlobWeb.SimpleLineRenderer.prototype.removeGeometry = function(geometry,style){
 	
-	for ( var i = 0; i<this.shapes.length; i++ )
+	for ( var i = 0; i<this.renderables.length; i++ )
 	{
-		var currentShape = this.shapes[i];
-		if ( currentShape.geometry == geometry){
-			this.shapes.splice(i, 1);
+		var currentRenderable = this.renderables[i];
+		if ( currentRenderable.geometry == geometry){
+			this.renderables.splice(i, 1);
 		}
 	}
 }
@@ -128,6 +170,8 @@ GlobWeb.SimpleLineRenderer.prototype.render = function(){
 
 	gl.disable(gl.DEPTH_TEST);
 	gl.enable(gl.BLEND);
+	gl.blendEquation(gl.FUNC_ADD);
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
 	this.program.apply();
 
@@ -135,11 +179,17 @@ GlobWeb.SimpleLineRenderer.prototype.render = function(){
 	mat4.multiply(renderContext.projectionMatrix, renderContext.viewMatrix, renderContext.modelViewMatrix)
 	gl.uniformMatrix4fv(this.program.uniforms["viewProjectionMatrix"], false, renderContext.modelViewMatrix);
 	
-	for ( var n = 0; n < this.shapes.length; n++ )
+	for ( var n = 0; n < this.renderables.length; n++ )
 	{
 		// opacity HACK
-		gl.uniform4f(this.program.uniforms["color"], this.shapes[n].style.strokeColor[0] , this.shapes[n].style.strokeColor[1], this.shapes[n].style.strokeColor[2], this.shapes[n].style.opacity / 2);
-		this.shapes[n].mesh.render(this.program.attributes);
+		gl.uniform4f(this.program.uniforms["color"], this.renderables[n].style.strokeColor[0] , this.renderables[n].style.strokeColor[1], this.renderables[n].style.strokeColor[2], this.renderables[n].style.opacity / 2.);
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.renderables[n].vertexBuffer);
+		gl.vertexAttribPointer(this.program.attributes['vertex'], this.renderables[n].vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.renderables[n].indexBuffer);
+		
+		gl.drawElements( gl.LINES, this.renderables[n].indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 	}
 	
 	gl.enable(gl.DEPTH_TEST);
@@ -154,5 +204,5 @@ GlobWeb.VectorRendererManager.registerRenderer({
 	creator: function(globe) { 
 			return new GlobWeb.SimpleLineRenderer(globe.tileManager);
 		},
-	canApply: function(type,style) {return (style.rendererHint == "Basic") && (type == "Polygon"); }
+	canApply: function(type,style) {return (style.rendererHint == "Basic") && (type == "Polygon" || type == "LineString") && (style.fill == false); }
 });
