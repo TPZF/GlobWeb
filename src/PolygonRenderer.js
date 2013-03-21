9504a1763f1e075fa66ab6e24a11ae592050a973
Fabien Lavignotte <Fabien.Lavignotte@telespazio.com>
2013-03-21 18:18:11 +0100

    Fix licence

--- src/PolygonRenderer.js
+++ src/PolygonRenderer.js
@@ -14,7 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with  If not, see <http://www.gnu.org/licenses/>.
+ * along with GlobWeb. If not, see <http://www.gnu.org/licenses/>.
  ***************************************/
  
 define( ['./CoordinateSystem','./VectorRendererManager','./FeatureStyle','./Program','./Triangulator'], 
